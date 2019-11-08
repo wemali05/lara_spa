31555df87310beaea4ecb87a7ce3c8e1e0071a8b
wemali05 <alfredwemali05@gmail.com>
2019-11-08 07:59:17 +0300

    set vue components and routes js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-require('./bootstrap');
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import routes from './routes'
+
+Vue.use(VueRouter);
+
+let app = new Vue({
+    el: '#app',
+
+    router: new VueRouter(routes)
+});
---
+++ resources/js/routes.js
@@ -0,0 +1,16 @@
+import Home from './components/Home';
+import About from './components/About';
+
+export default {
+    mode: 'history',
+
+    routes: [{
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/about',
+            component: About
+        }
+    ]
+};
